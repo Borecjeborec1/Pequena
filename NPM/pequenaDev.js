f989ebab9a2f479a0af53862fda945d2a358a2d2
borecjeborec1 <robingrundel@seznam.cz>
2023-04-07 19:55:26 +0200

    handling user close

--- NPM/pequenaDev.js
+++ NPM/pequenaDev.js
@@ -4,15 +4,21 @@ const chokidar = require('chokidar');
 let pyProcess = null;
 
 function startPyProcess() {
-  pyProcess = spawn('__PYTHONPATH__', ['-u', 'index.py']);
+  pyProcess = spawn('python', ['-u', 'index.py']);
   pyProcess.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
+  });
+  pyProcess.on('exit', (code, signal) => {
+    if (code === 0 && signal === null) {
+      console.log(`Python process closed by user. Qutting Dev reload`);
+      process.exit();
+    }
   });
 }
 
 startPyProcess();
 
-const watcher = chokidar.watch('.');
+const watcher = chokidar.watch('./client');
 
 watcher.on('change', () => {
   console.log('Changes detected, restarting Python process...');
