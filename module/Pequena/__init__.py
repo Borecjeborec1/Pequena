70174aef65ca6ddac697289b2a4e173dd77a8b59
borecjeborec1 <robingrundel@seznam.cz>
2023-04-27 19:35:56 +0200

    v20

--- module/Pequena/__init__.py
+++ module/Pequena/__init__.py
@@ -1,9 +1,5 @@
 from Pequena.window import init
 from Pequena.window import create_window
-from Pequena.window import expose_functions
+from Pequena.window import start
 
-from Pequena.window import set_CLOSABLE
-from Pequena.window import get_CLOSABLE
-from Pequena.window import set_HIDE_ON_CLOSE
-from Pequena.window import get_HIDE_ON_CLOSE
-from Pequena.window import toggleHide
+from Pequena.window import expose_functions
--- module/setup.py
+++ module/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.27"
+VERSION = "0.0.29"
 DESCRIPTION = 'Lightweight desktop app framework'
 with open("Readme.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
