0677d51ea2012c99edf6b5de950216fff7a533f3
borecjeborec1 <robingrundel@seznam.cz>
2023-05-03 21:13:49 +0200

    revet

--- module/setup.py
+++ module/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.44"
+VERSION = "0.0.38"
 DESCRIPTION = 'Lightweight desktop app framework'
 with open("Readme.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
@@ -14,16 +14,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[
-        'pythonnet ; sys_platform == "win32"',
-        'pyobjc-core ; sys_platform == "darwin"',
-        'pyobjc-framework-Cocoa ; sys_platform == "darwin"',
-        'pyobjc-framework-WebKit ; sys_platform == "darwin"',
-        'QtPy ; sys_platform == "openbsd6"',
-        'importlib_resources; python_version < "3.7"',
-        'proxy_tools',
-        'bottle'
-    ],
+    install_requires=['pywebview'],
     keywords=['python'],
     classifiers=[
         "Development Status :: 1 - Planning",
